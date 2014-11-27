dfe94f51374770b9763a3e9667667e959910bb9f
JimmyReload <jimmy@uplinkzero.com>
2014-11-27 14:04:05 +0000

    Updated project with more debugging

--- ZeroMQ-testing/ZeroMQ-pub/main.cpp
+++ ZeroMQ-testing/ZeroMQ-pub/main.cpp
@@ -10,15 +10,18 @@ std::string eth0IP = "10.172.132.58";
 */
 int main()
 {
-    
     // Set up the connection
     zmq::context_t context(1);
-    zmq::socket_t * socket = new zmq::socket_t(context, ZMQ_PUB);
+    zmq::socket_t * pZmqSocket = new zmq::socket_t(context, ZMQ_PUB);
     std::string publishAddress = "epgm://" + eth0IP + ";239.192.1.1:5555";
-    //std::string publishAddress = "tcp://*:5555";
-    socket->bind(publishAddress.c_str());
-    //int linger = 0;
-    //socket->setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
+    try
+    {
+        pZmqSocket->bind(publishAddress.c_str());
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception caught during socket.bind(): " << e.what() << std::endl;
+    }
 
     // Send the message
     while (1)
@@ -26,24 +29,32 @@ int main()
         std::cout << "Sending Heartbeat Message" << std::endl;
         std::string szMessageData = "Foo";
         
-        zmq::message_t zmqMsgOut(sizeof(szMessageData) + 1);
-        memcpy((void *)zmqMsgOut.data(), szMessageData.c_str(), sizeof(szMessageData));
+        zmq::message_t * zmqMsgOut = new zmq::message_t(sizeof(szMessageData) + 1);
+        memcpy((void *)zmqMsgOut->data(), szMessageData.data(), sizeof(szMessageData));
         
-        bool status = socket->send(zmqMsgOut);
-        if (status) {
-            std::cout << "Heartbeat Message Sent." << std::endl;
-        } 
-        else {
-            std::cerr << "Heartbeat Message NOT Sent." << std::endl;
+        try
+        {
+            if (pZmqSocket->send(*zmqMsgOut))
+            {
+                std::cout << "Heartbeat Message Sent." << std::endl;
+            }
+            else
+            {
+                std::cerr << "Heartbeat Message NOT Sent." << std::endl;
+            }
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << "Exception caught during socket.send(): " << e.what() << std::endl;
         }
 
         Sleep(1000);
     }
 
     // Close and delete the socket
-    socket->disconnect(publishAddress.c_str());
-    socket->close();
-    delete socket;
+    pZmqSocket->disconnect(publishAddress.c_str());
+    pZmqSocket->close();
+    delete pZmqSocket;
 
     return 0;
 }
--- ZeroMQ-testing/ZeroMQ-sub/main.cpp
+++ ZeroMQ-testing/ZeroMQ-sub/main.cpp
@@ -13,8 +13,14 @@ int main()
     zmq::context_t zmqContext(1);
     zmq::socket_t * pZmqSocket = new zmq::socket_t(zmqContext, ZMQ_SUB);
     std::string subscribeAddress = "epgm://" + eth0IP + ";239.192.1.1:5555";
-    //std::string subscribeAddress = "tcp://localhost:5556";
-    pZmqSocket->connect(subscribeAddress.c_str());
+    try
+    {
+        pZmqSocket->connect(subscribeAddress.c_str());
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception caught during socket.connect(): " << e.what() << std::endl;
+    }
     pZmqSocket->setsockopt(ZMQ_SUBSCRIBE, "", 0);
     std::cout << "Heartbeat responder BOUND" << std::endl;
     while (1)
@@ -23,15 +29,24 @@ int main()
         //pZmqSocket->connected();
         zmq::message_t * pZmqMsgIn = new zmq::message_t();
 
-        bool success = pZmqSocket->recv(pZmqMsgIn);
-        if (success) {
-            std::cout << "======== MESSAGE RECEIVED =========" << std::endl;
+        try
+        {
+            if (pZmqSocket->recv(pZmqMsgIn)) 
+            {
+                std::cout << "======== MESSAGE RECEIVED =========" << std::endl;
+            }
+            else 
+            {
+                std::cerr << "Error receiving message" << std::endl;
+            }
         }
-        else {
-            std::cerr << "Error receiving message" << std::endl;
+        catch (std::exception& e)
+        {
+            std::cerr << "Exception caught during socket.send(): " << e.what() << std::endl;
         }
 
         //std::string * szMessageData = static_cast<std::string *>(pZmqMsgIn->data());
+        Sleep(1000);
 
         delete pZmqMsgIn;
     }
